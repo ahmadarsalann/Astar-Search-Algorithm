7e50b1a8370b95affe2ac3999284a545718c9576
arsalan231 <aahmad3@mail.umw.edu>
2021-09-11 20:25:39 -0400

    Changed it to my name on September 11

--- gps.py
+++ gps.py
@@ -2,7 +2,7 @@
 
 '''
 CPSC 415 -- Homework #2 template
-Stephen Davies, University of Mary Washington, fall 2021
+Arsalan Ahmad, University of Mary Washington, fall 2021
 '''
 
 from atlas import Atlas
